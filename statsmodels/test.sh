aa42e011083ecb040260a9307b3127a507429b9c
TankMermaid <xnzhang@genetics.ac.cn>
2017-07-26 21:42:29 +0800

    commit direction

---
+++ statsmodels/test.sh
@@ -0,0 +1,8 @@
+#########################################################################
+# File Name: test.sh
+# Author: Tank
+# mail: xnzhang@genetics.ac.cn
+# Created Time: Wed 26 Jul 2017 09:39:49 PM CST
+#########################################################################
+#!/bin/bash
+##  this file is tested for use of something 
